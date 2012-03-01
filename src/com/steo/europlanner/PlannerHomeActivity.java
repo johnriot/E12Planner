498fa67bb6818a2a3f0a2b5a2a04ab793e2db559
Stephen Roberts <stephen.roberts4@gmail.com>
2012-03-01 22:17:37 +0100

    Menu with debug colours, misaligned, and wrong icons

--- src/com/steo/europlanner/PlannerHomeActivity.java
+++ src/com/steo/europlanner/PlannerHomeActivity.java
@@ -2,13 +2,17 @@ package com.steo.europlanner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.LinearLayout;
 
 public class PlannerHomeActivity extends Activity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.main);
 
         LinearLayout mainLayout = (LinearLayout)findViewById(R.id.mainLayout);
