ea637086cae470260ef4ddec349a36592e3870e8
Stephen Roberts <stephen.roberts4@gmail.com>
2012-03-01 23:31:36 +0100

    Making buttons on menu clickable. providing activity for games and toasts for other not implemented

---
+++ src/com/steo/europlanner/GamesActivity.java
@@ -0,0 +1,18 @@
+package com.steo.europlanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class GamesActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.games_layout);
+    }
+
+}
--- src/com/steo/europlanner/PlannerHomeActivity.java
+++ src/com/steo/europlanner/PlannerHomeActivity.java
@@ -1,11 +1,15 @@
 package com.steo.europlanner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class PlannerHomeActivity extends Activity {
+public class PlannerHomeActivity extends Activity implements OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,38 @@ public class PlannerHomeActivity extends Activity {
 
         LinearLayout mainLayout = (LinearLayout)findViewById(R.id.mainLayout);
         mainLayout.getBackground().setAlpha(100);
+
+        LinearLayout factfileLayout =
+                (LinearLayout)findViewById(R.id.factfile_button);
+        factfileLayout.setOnClickListener(this);
+
+        LinearLayout teamsLayout =
+                (LinearLayout)findViewById(R.id.teams_button);
+        teamsLayout.setOnClickListener(this);
+
+        LinearLayout venuesLayout =
+                (LinearLayout)findViewById(R.id.venues_button);
+        venuesLayout.setOnClickListener(this);
+
+        LinearLayout gamesLayout =
+                (LinearLayout)findViewById(R.id.games_button);
+        gamesLayout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+        case R.id.games_button:
+
+            Intent intent = new Intent(this, GamesActivity.class);
+            startActivity(intent);
+
+            break;
+        case R.id.teams_button:
+        case R.id.venues_button:
+        case R.id.factfile_button:
+        default:
+            Toast.makeText(this, "Not implemented yet.", Toast.LENGTH_LONG).show();
+        }
     }
 }
