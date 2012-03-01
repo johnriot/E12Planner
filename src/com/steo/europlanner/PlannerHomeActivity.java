5c1b3d7e4b9e54f617776c405e2a06788d75af73
Stephen Roberts <stephen.roberts@isis-papyrus.com>
2012-03-01 17:23:30 +0100

    Initial commit

---
+++ src/com/steo/europlanner/PlannerHomeActivity.java
@@ -0,0 +1,17 @@
+package com.steo.europlanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class PlannerHomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        LinearLayout mainLayout = (LinearLayout)findViewById(R.id.mainLayout);
+        mainLayout.getBackground().setAlpha(100);
+    }
+}
