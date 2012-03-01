05a450a29bb059dfa1e5015a19e6db8e4db30e22
Stephen Roberts <stephen.roberts@isis-papyrus.com>
2012-03-01 18:55:18 +0100

    Fixing bug as per http://stackoverflow.com/questions/3904852/android-layout-broken-with-9-patch-background and adding football icon

--- gen/com/steo/europlanner/R.java
+++ gen/com/steo/europlanner/R.java
@@ -15,15 +15,19 @@ public final class R {
         public static final int white=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int footballicon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int semitrans_rounded_corner=0x7f020003;
     }
     public static final class id {
         public static final int backgroundLayout=0x7f060000;
+        public static final int buttonsMenu=0x7f060002;
         public static final int mainLayout=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int menuitem=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
