c37f10a56aa0184ba8d0aa051288d33d569f3c12
Stephen Roberts <stephen.roberts4@gmail.com>
2012-03-08 23:41:40 +0100

    adding link for how to do quick actions menu

--- src/com/steo/europlanner/TeamsActivity.java
+++ src/com/steo/europlanner/TeamsActivity.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 
 public class TeamsActivity extends FragmentActivity {
 
+    //Link for quick action menu: http://www.londatiga.net/it/how-to-create-quickaction-dialog-in-android/
     private ArrayList<String> mTeams;
 
     @Override
