7ba9011e0dcbc55845764e5eb1a962dfae1e91f1
John O'Halloran <ohalloran.john@gmail.com>
2012-05-22 21:27:04 +0900

    Tables implemented to give actual points etc based on results

--- src/com/steo/europlanner/Fixture.java
+++ src/com/steo/europlanner/Fixture.java
@@ -52,4 +52,49 @@ public class Fixture {
     public boolean involvesTeams(ArrayList<Team> teams) {
         return teams.contains(mHomeTeam) && teams.contains(mAwayTeam);
     }
+
+    public boolean involvesTeam(Team team) {
+        return team.equals(mHomeTeam) || team.equals(mAwayTeam);
+    }
+
+    public boolean hasGameBeenPlayed() {
+       return mScore.hasGameBeenPlayed();
+    }
+
+    public int getGoalsFor(Team team) {
+        if(hasGameBeenPlayed()) {
+            if(team.equals(mHomeTeam)) {
+                return mScore.getHomeScore();
+            }
+            else { // awayTeam
+                return mScore.getAwayScore();
+            }
+        }
+        return 0;
+    }
+
+    public int getGoalsAgainst(Team team) {
+        if(hasGameBeenPlayed()) {
+            if(team.equals(mHomeTeam)) {
+                return mScore.getAwayScore();
+            }
+            else { // awayTeam
+                return mScore.getHomeScore();
+            }
+        }
+        return 0;
+    }
+
+    public int getPoints(Team team) {
+        if(hasGameBeenPlayed()) {
+            if(team.equals(mHomeTeam)) {
+                return mScore.getHomePoints();
+            }
+            else {
+                return mScore.getAwayPoints();
+            }
+        }
+        return 0;
+    }
+
 }
