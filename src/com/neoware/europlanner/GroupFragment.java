c304fdb56225d7cc2a96f4ed25ca72be1d4f90b3
Stephen Roberts <stephen.roberts4@gmail.com>
2012-05-28 13:58:21 +0200

    Rest of fixtures layout for groups

--- src/com/neoware/europlanner/GroupFragment.java
+++ src/com/neoware/europlanner/GroupFragment.java
@@ -90,6 +90,8 @@ public class GroupFragment extends SherlockFragment {
 
         TableLayout fixturesTable = (TableLayout)fragView.findViewById(R.id.fixturesTable);
 
+        String scoreSep = res.getString(R.string.scoreSeperator);
+
         ArrayList<Fixture> fixtures = mStage.getFixtures();
         Date currentDate = null;
         for(Fixture fixture : fixtures) {
@@ -110,11 +112,34 @@ public class GroupFragment extends SherlockFragment {
             TableRow fixtureRow = (TableRow) inflater.inflate(
                     R.layout.fixtures_table_fixture_row, null);
 
-            TextView fixtureView = (TextView)fixtureRow.findViewById(
-                    R.id.fixturesTableFixture);
-            fixtureView.setText("Do proper layout: " +
-                    teamNames[fixture.getHomeTeamId()] + " v " +
-                    teamNames[fixture.getAwayTeamId()]);
+            TextView homeTeamTV = (TextView)fixtureRow.findViewById(
+                    R.id.fixturesTableHomeTeam);
+            homeTeamTV.setText(teamNames[fixture.getHomeTeamId()]);
+
+            ImageView homeTeamIV = (ImageView)fixtureRow.findViewById(
+                    R.id.fixturesTableHomeTeamIcon);
+            homeTeamIV.setImageResource(res.getIdentifier(crestIds[fixture.getHomeTeamId()],
+                    "drawable", packageName));
+
+            TextView awayTeamTV = (TextView)fixtureRow.findViewById(
+                    R.id.fixturesTableAwayTeam);
+            awayTeamTV.setText(teamNames[fixture.getAwayTeamId()]);
+
+            ImageView awayTeamIV = (ImageView)fixtureRow.findViewById(
+                    R.id.fixturesTableAwayTeamIcon);
+            awayTeamIV.setImageResource(res.getIdentifier(crestIds[fixture.getAwayTeamId()],
+                    "drawable", packageName));
+
+            TextView scoreTv = (TextView)fixtureRow.findViewById(
+                    R.id.fixturesTableScore);
+            Score score = fixture.getScore();
+            if(score.hasGameBeenPlayed()) {
+                String scoreStr = score.getHomeScore() + scoreSep + score.getAwayScore();
+                scoreTv.setText(scoreStr);
+            }
+            else {
+                scoreTv.setText(scoreSep);
+            }
 
             fixturesTable.addView(fixtureRow);
         }
