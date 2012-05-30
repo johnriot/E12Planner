998507fcb05261de09eb1577c9fa1d41e6ee58b6
John O'Halloran <ohalloran.john@gmail.com>
2012-05-31 03:52:10 +0900

    Implemented the innards of Knockout so that it will read the relevant teams from the xml file and display them in the knockout stages.

--- src/com/neoware/europlanner/Knockout.java
+++ src/com/neoware/europlanner/Knockout.java
@@ -11,6 +11,7 @@ import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ import android.widget.TextView;
 public class Knockout extends TournamentStage {
 
     private static final int KNOCKOUT_FRAGMENT_RESOURCE = R.layout.knockout_fragment;
+    public static final int KNOCKOUT_ID_OFFSET = 100;
 
     public Knockout(int id) {
         super(id);
@@ -71,26 +73,59 @@ public class Knockout extends TournamentStage {
                 currentDate = fixture.getTime();
             }
 
-            TableRow fixtureRow = (TableRow) inflater.inflate(
-                    R.layout.knockout_table_fixture_ph_row, null);
+            TableRow fixtureRow = null;
+            // Decide which Teams are Real and which are Placeholders; draw view accordingly
+            if(fixture.getHomeTeam().isRealTeam() && fixture.getAwayTeam().isRealTeam()) {
+                fixtureRow = (TableRow) inflater.inflate(
+                        R.layout.fixtures_table_fixture_row, null);
+            }
+            else if(fixture.getHomeTeam().isRealTeam()) {
+                fixtureRow = (TableRow) inflater.inflate(
+                        R.layout.knockout_t_f_left_real, null);
+            }
+            else if(fixture.getAwayTeam().isRealTeam()) {
+                fixtureRow = (TableRow) inflater.inflate(
+                        R.layout.knockout_t_f_right_real, null);
+            }
+            else { // both real teams (not placeholders)
+                fixtureRow = (TableRow) inflater.inflate(
+                        R.layout.knockout_t_f_neither_real, null);
+            }
+
+            String crestIds[] = res.getStringArray(R.array.team_icon_resource_names);
+            String teamNames[] = res.getStringArray(R.array.team_names);
+            String packageName = getClass().getPackage().getName();
 
             TextView homeTeamTV = (TextView)fixtureRow.findViewById(
                     R.id.fixturesTableHomeTeam);
-            homeTeamTV.setText(teamPlaceholders[fixture.getHomeTeamId()]);
 
-            //ImageView homeTeamIV = (ImageView)fixtureRow.findViewById(
-            //        R.id.fixturesTableHomeTeamIcon);
-            //homeTeamIV.setImageResource(res.getIdentifier(crestIds[fixture.getHomeTeamId()],
-            //        "drawable", packageName));
+
+            if(fixture.getHomeTeam().isRealTeam()) {
+                homeTeamTV.setText(teamNames[fixture.getHomeTeamId()]);
+                ImageView homeTeamIV = (ImageView)fixtureRow.findViewById(
+                        R.id.fixturesTableHomeTeamIcon);
+                homeTeamIV.setImageResource(res.getIdentifier(crestIds[fixture.getHomeTeamId()],
+                        "drawable", packageName));
+            }
+            else {
+                homeTeamTV.setText(teamPlaceholders[fixture.getHomeTeamId() - KNOCKOUT_ID_OFFSET]);
+            }
+
 
             TextView awayTeamTV = (TextView)fixtureRow.findViewById(
                     R.id.fixturesTableAwayTeam);
-            awayTeamTV.setText(teamPlaceholders[fixture.getAwayTeamId()]);
 
-            //ImageView awayTeamIV = (ImageView)fixtureRow.findViewById(
-            //        R.id.fixturesTableAwayTeamIcon);
-            //awayTeamIV.setImageResource(res.getIdentifier(crestIds[fixture.getAwayTeamId()],
-            //        "drawable", packageName));
+
+            if(fixture.getAwayTeam().isRealTeam()) {
+                awayTeamTV.setText(teamNames[fixture.getAwayTeamId()]);
+                ImageView awayTeamIV = (ImageView)fixtureRow.findViewById(
+                        R.id.fixturesTableAwayTeamIcon);
+                awayTeamIV.setImageResource(res.getIdentifier(crestIds[fixture.getAwayTeamId()],
+                        "drawable", packageName));
+            }
+            else {
+                awayTeamTV.setText(teamPlaceholders[fixture.getAwayTeamId() - KNOCKOUT_ID_OFFSET]);
+            }
 
             TextView scoreTv = (TextView)fixtureRow.findViewById(
                     R.id.fixturesTableScore);
