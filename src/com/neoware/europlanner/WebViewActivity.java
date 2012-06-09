db12969de577e713c5b61e943cccb759a87dc7f3
Stephen Roberts <stephen.roberts4@gmail.com>
2012-06-09 11:37:07 +0200

    Adding indeterminate spinner to web view

--- src/com/neoware/europlanner/WebViewActivity.java
+++ src/com/neoware/europlanner/WebViewActivity.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import junit.framework.Assert;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Shader.TileMode;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.webkit.WebViewClient;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.Window;
 import com.google.ads.AdRequest;
 import com.google.ads.AdView;
 
@@ -27,15 +29,37 @@ public class WebViewActivity extends SherlockActivity {
      * within the webview itself
      */
     private class LocalRedirectClient extends WebViewClient {
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                String description, String failingUrl) {
+
+            super.onReceivedError(view, errorCode, description, failingUrl);
+            setSupportProgressBarIndeterminateVisibility(false);
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return false;
         }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            setSupportProgressBarIndeterminateVisibility(false);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            setSupportProgressBarIndeterminateVisibility(true);
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.webview);
